a98b56cb5289550af6a234f73f2dd713104813c7
Berk Genc <berkgenc@gmail.com>
2019-03-05 00:45:20 +0300

    Add project files.

---
+++ University/Controllers/StudentsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using University.Models;
+
+namespace University.Controllers
+{
+    public class StudentsController : Controller
+    {
+        UniversityContext universityContext;
+
+        public StudentsController(UniversityContext context)
+        {
+            universityContext = context;
+
+        }
+
+        public IActionResult Index()
+        {
+            var students = universityContext.Students.ToList();
+            return View(students);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Create(Student model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            universityContext.Add(model);
+            universityContext.SaveChanges();
+            return RedirectToAction("index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var emp = universityContext.Students.Find(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            universityContext.Students.Update(model);
+            universityContext.SaveChanges();
+            return RedirectToAction("index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var emp = universityContext.Students.Find(id);
+            universityContext.Students.Remove(emp);
+            universityContext.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            Student sampleStudent = universityContext.Students.Where(b => b.Id == id).FirstOrDefault();
+            if (sampleStudent != null)
+            {
+                return View(sampleStudent);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
---
+++ University/Migrations/20190304202759_decimal.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace University.Migrations
+{
+    public partial class @decimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "GPA",
+                table: "Students",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "GPA",
+                table: "Students",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
